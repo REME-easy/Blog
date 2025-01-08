b0e0cefa85bae952b521a5c603934d91035a0eb7
REME <asreme@qq.com>
2025-01-08 21:08:14 +0800

    add comments

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,22 +1,69 @@
-'use client'
+"use client";
 
-import { Comments as CommentsComponent } from 'pliny/comments'
-import { useState } from 'react'
-import siteMetadata from '@/data/siteMetadata'
+import { useTheme } from "next-themes";
+import GiscusComponent, { BooleanString, InputPosition, Mapping } from "@giscus/react";
+import siteMetadata from "@/data/siteMetadata";
 
-export default function Comments({ slug }: { slug: string }) {
-  const [loadComments, setLoadComments] = useState(false)
+interface GiscusConfigs {
+  themeURL: string;
+  theme: string;
+  darkTheme: string;
+  mapping: Mapping;
+  repo: `${string}/${string}`;
+  repositoryId: string;
+  category: string;
+  categoryId: string;
+  reactions: BooleanString;
+  metadata: BooleanString;
+  inputPosition: InputPosition;
+  lang: string;
+}
 
-  if (!siteMetadata.comments?.provider) {
-    return null
-  }
+
+interface CommentsProps {
+  className?: string;
+  configs?: Partial<GiscusConfigs>;
+}
+
+export default function Comments(props: CommentsProps) {
+  const { configs, className } = props;
+
+  const defaultConfigs = siteMetadata.comments.giscusConfig as GiscusConfigs;
+  const {
+    themeURL,
+    theme,
+    darkTheme,
+    repo,
+    repositoryId,
+    category,
+    categoryId,
+    reactions,
+    metadata,
+    inputPosition,
+    lang,
+    mapping,
+  } = { ...defaultConfigs, ...configs };
+
+  const { theme: siteTheme, resolvedTheme } = useTheme();
+  const commentsTheme =
+    themeURL === "" ? (siteTheme === "dark" || resolvedTheme === "dark" ? darkTheme : theme) : themeURL;
+
   return (
-    <>
-      {loadComments ? (
-        <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />
-      ) : (
-        <button onClick={() => setLoadComments(true)}>Load Comments</button>
-      )}
-    </>
-  )
+    <div id="comments" className={className}>
+      <GiscusComponent
+        id="comments-container"
+        repo={repo}
+        repoId={repositoryId}
+        category={category}
+        categoryId={categoryId}
+        mapping={mapping}
+        reactionsEnabled={reactions}
+        emitMetadata={metadata}
+        inputPosition={inputPosition}
+        theme={commentsTheme}
+        lang={lang}
+        loading="lazy"
+      />
+    </div>
+  );
 }
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,4 +1,3 @@
-/** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
   title: 'Glitched Space',
   author: 'Reme',
